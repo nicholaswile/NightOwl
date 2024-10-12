f312fb8936ba1234cf892e357b8e8ca624741db9
Nicholas Wile <74445404+nicholaswile@users.noreply.github.com>
2024-10-12 15:09:48 -0400

    Edit main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Welcome to NightOwl! Your 24/7 AI Student Assistant")
+    print("Welcome to NightOwl! Your 24/7 AI Student Assistant for KSU")
 
 if __name__ == "__main__":
     main()
